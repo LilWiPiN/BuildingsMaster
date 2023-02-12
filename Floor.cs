ebe9ee1ae0d10c4ecc69a39d880b1cebb3ae82b0
Valentin Arias <valentin.arias@upb.edu.co>
2023-02-12 18:01:42 -0500

    Diagrama de clases

---
+++ Floor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingsMaster
+{
+    public class Floor
+    {
+        private List<Character> characters = new List<Character>();
+        private List<Floor> floors = new List<Floor>();
+
+        public void AddCharacter()
+        {
+
+        }
+
+        public void RemoveCharacter()
+        {
+
+        }
+    }
+}
---
+++ Tower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingsMaster
+{
+    public class Tower
+    {
+        private List<Floor> floors = new List<Floor>();
+
+        public void AddFloor()
+        {
+
+        }
+
+        public void RemoveFloor()
+        {
+
+        }
+    }
+}
